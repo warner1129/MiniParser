7e5841def2d24657fe474a10eb7b52d3f0517eda
warner1129 <warner1129@gmail.com>
2025-01-27 03:46:43 +0800

    upd ast.cpp

--- src/ast.cpp
+++ src/ast.cpp
@@ -3,21 +3,42 @@
 
 namespace miniparser {
 
-// '|' "─" '└' '├' '┬' '│'
+void printAST(std::ostream &out, std::shared_ptr<ASTnode> root) {
+    static const std::string patterns[4] {
+        "│   ",
+        "├───",
+        "└───",
+        "    ",
+    };
 
-void dfs(std::shared_ptr<ASTnode> ptr, std::string tab) {
-    std::cerr << tab << ptr->display << '\n';
-    if (!tab.empty()) {
-        if (tab.substr(tab.size() - 12, 3) == "├") {
-            tab = tab.substr(0, tab.size() - 12) + "│   ";
-        } else if (tab.substr(tab.size() - 12, 3) == "└") {
-            tab = tab.substr(0, tab.size() - 12) + "    ";
+    std::vector<int> tab;
+    auto dfs = [&](auto &&self, std::shared_ptr<ASTnode> ptr) -> void {
+        for (int i : tab) {
+            out << patterns[i];
         }
-    }
-    for (auto sub : ptr->children) {
-        bool last = (sub == ptr->children.back());
-        dfs(sub, tab + (last ? "└───" : "├───"));
-    }
+        out << ptr->display << '\n';
+        int last = -1;
+        if (!tab.empty()) {
+            last = tab.back();
+            if (last == 1) {
+                tab.back() = 0;
+            } else if (last == 2) {
+                tab.back() = 3;
+            }
+        }
+        tab.push_back(1);
+        for (auto sub : ptr->children) {
+            if (sub == ptr->children.back()) {
+                tab.back() = 2;
+            }
+            self(self, sub);
+        }
+        tab.pop_back();
+        if (last != -1) {
+            tab.back() = last;
+        }
+    };
+    dfs(dfs, root);
 }
 
 } // namespace miniparser
